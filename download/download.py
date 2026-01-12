25ea6b4ac07b74eff912a744a6093668e97034b6
Adeel Qureshi <adeelqureshi@JC47H6MWCY-Adeel-Qureshi.local>
2026-01-12 12:49:54 -0500

    added ds args to download

--- download/download.py
+++ download/download.py
@@ -4,9 +4,24 @@ import tiktoken
 from datasets import load_dataset
 import numpy as np
 
-dataset_name = "HuggingFaceFW/fineweb-edu"
-local_dir = "edu_fineweb10B"
-remote_name = "sample-10BT"
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--ds', type=str, choices=["fw", "ts"], help='Dataset to download: fw (FineWeb Edu) | ts (The Stack)')
+
+args = parser.parse_args()
+if args.ds == "fw":
+    dataset_name = "HuggingFaceFW/fineweb-edu"
+    remote_name = "sample-10BT"
+    local_dir = "edu_fineweb10B"
+elif args.ds == "ts":
+    dataset_name = "roneneldan/TinyStories"
+    remote_name = ""
+    local_dir = "tinystories"
+else:
+    raise ValueError("Invalid dataset choice. Use --ds fw or --ds ts")
+
 shard_size = int(1e8) # 100M tokens per shard
 
 # create the dir
@@ -21,7 +36,6 @@ def tokenize(doc):
     tokens.extend(tokenizer.encode_ordinary(doc["text"]))
     tokens_np = np.array(tokens, dtype=np.uint16)
     return tokens_np
-
 
 def main():
     # download dataset inside main
@@ -48,6 +62,7 @@ def main():
                 remainder = shard_size - token_count
                 all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
                 np.save(filename, all_tokens_np)
+                print(f"Saved shard {filename} with {shard_size:,} tokens")
 
                 shard_index += 1
 
@@ -56,13 +71,16 @@ def main():
                 all_tokens_np[0:leftover] = tokens[remainder:]
                 token_count = leftover
 
+
         # final shard
         if token_count != 0:
             split = "val" if shard_index == 0 else "train"
             filename = os.path.join(local_dir, f"{split}_{shard_index:06d}")
             np.save(filename, all_tokens_np[:token_count])
-
+            print(f"Saved final shard {filename} with {token_count:,} tokens")
 
 if __name__ == "__main__":
-    mp.freeze_support()  # optional on macOS, but safe
     main()
+    print("Download and tokenization complete.")
+
+
